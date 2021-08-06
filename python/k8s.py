8ade7af96c42cae93d9cb51473c18636a1c0484e
Harold Armijo Leon <harmijoleon@theblockchain-group.com>
2021-08-06 18:23:14 +0200

    kubectl buildkit added

--- python/k8s.py
+++ python/k8s.py
@@ -72,6 +72,7 @@ k3d_url = 'https://github.com/rancher/k3d/releases/download/v4.4.4/k3d-linux-amd
 kind_url = 'https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64'
 helm_url = 'https://get.helm.sh/helm-v3.6.0-linux-amd64.tar.gz'
 kubectl_url = 'https://dl.k8s.io/release/v1.21.2/bin/linux/amd64/kubectl'
+kubectl_buildkit_url = 'https://github.com/vmware-tanzu/buildkit-cli-for-kubectl/releases/download/v0.1.3/linux-v0.1.3.tgz'
 tilt_url = 'https://github.com/tilt-dev/tilt/releases/download/v0.22.3/tilt.0.22.3.linux.x86_64.tar.gz'
 kind_config = """
 kind: Cluster
@@ -246,6 +247,33 @@ def kubectl(force):
     else:
         LOGGER.info("No need to install kubectl, force with --force")
 
+@install_dependency.command()
+@flag('--force', help="Overwrite the existing binaries")
+def kubectl_buildkit(force):
+    """Install kubectl buildkit"""
+    kubectl_buildkit_version = re.search('/(v[0-9.]+)/', kubectl_buildkit_url).group(1)
+    found_kubectl_buildkit_version = False
+    try:
+        found_kubectl_buildkit_version = check_output(['kubectl', 'buildkit', 'version'])
+        found_kubectl_buildkit_version = re.sub(r'\n', '', found_kubectl_buildkit_version)
+    except subprocess.CalledProcessError:
+        found_kubectl_buildkit_version = False
+
+    if not force and not found_kubectl_buildkit_version:
+        force = True
+        LOGGER.info("Could not find kubectl buildkit")
+    if not force and found_kubectl_buildkit_version != kubectl_buildkit_version:
+        force = True
+        LOGGER.info(
+            f"Found an older version of kubectl buildkit ({found_kubectl_buildkit_version}) than the requested one {kubectl_buildkit_version}")
+    if force:
+        with tempdir() as d:
+            extract(kubectl_buildkit_url, d)
+            move(Path(d) / 'kubectl-build', bin_dir / 'kubectl-build')
+            move(Path(d) / 'kubectl-buildkit', bin_dir / 'kubectl-buildkit')
+    else:
+        LOGGER.info("No need to install kubectl buildkit, force with --force")
+
 
 @install_dependency.command()
 @flag('--force', help="Overwrite the existing binaries")
@@ -253,6 +281,7 @@ def _all(force):
     """Install all the dependencies"""
     ctx = click.get_current_context()
     ctx.invoke(kubectl, force=force)
+    ctx.invoke(kubectl_buildkit, force=force)
     ctx.invoke(helm, force=force)
     ctx.invoke(tilt, force=force)
     ctx.invoke(k3d, force=force)
