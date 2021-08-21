a4725a83f6ca34c66d11d3e357355910bddbc600
Gaëtan Lehmann <gaetan.lehmann@gmail.com>
2021-08-21 17:24:24 +0200

    fix remaining .context()

--- python/k8s.py
+++ python/k8s.py
@@ -290,7 +290,7 @@ def install_cert_manager(version):
     """Install a certificate manager in the current cluster"""
     call(['helm', 'repo', 'add', 'jetstack', 'https://charts.jetstack.io'])
     call([
-        'helm', '--kube-context', config.kubectl.context(),
+        'helm', '--kube-context', config.kubectl.context,
         'upgrade', '--install', '--create-namespace', '--wait', 'cert-manager', 'jetstack/cert-manager',
         '--namespace', 'cert-manager',
         '--version', version,
