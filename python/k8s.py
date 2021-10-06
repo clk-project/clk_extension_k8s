c73b32eadcf0d3db216e9013b9ac0ef8c2183b5d
Samuel Loury <konubinixweb@gmail.com>
2021-10-06 16:42:03 +0200

    Only consider the package as being loosely matched when it is not an exact match

--- python/k8s.py
+++ python/k8s.py
@@ -717,7 +717,8 @@ def helm_dependency_update(path, force, touch, experimental_oci, packages, remov
             depArchives.add(name)
             deps_to_update.append(dep)
         elif matched_generated_packages:
-            LOGGER.warning(f"{name} loosely matched to package {matched_generated_packages[0]}")
+            if name != matched_generated_packages[0]:
+                LOGGER.warning(f"{name} loosely matched to package {matched_generated_packages[0]}")
             depArchives.add(matched_generated_packages[0])
         elif os.path.exists(f'{path}/charts/{name}'):
             depArchives.add(name)
